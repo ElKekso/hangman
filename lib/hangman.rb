8caf11ba6fe0a767cd70a88912579d5395b9012d
ElKekso <159456154+ElKekso@users.noreply.github.com>
2025-02-05 19:12:04 +0100

    Add get_guess and show_progress

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,32 @@
+guessed_letters = Hash.new
+word = pick_word
+
 def pick_word
   words = File.readlines('google-10000-english-no-swears.txt')
   words.sample
 end
 
-puts pick_word
+def get_guess
+  puts "Please enter a single letter you did not guess yet:"
+  while true
+    ret = gets.chomp
+    return ret if ret.count("a-zA-Z") > 0 && !guessed_letters.has_key?(ret)
+  end
+end
+
+def show_progress
+  word.chars.each {|char| 
+  if guessed_letters.has_key?(char)
+    print char
+  else
+    print '_'
+  end
+  #$stdout.flush 
+}
+end
+
+
+
 
 
 
