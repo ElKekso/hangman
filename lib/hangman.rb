e9b0332258d8d171c7fb75a3a4e1992b31d7b0f0
ElKekso <159456154+ElKekso@users.noreply.github.com>
2025-02-05 19:18:28 +0100

    Adjust show_progress

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,10 +1,10 @@
-guessed_letters = Hash.new
-word = pick_word
-
 def pick_word
   words = File.readlines('google-10000-english-no-swears.txt')
   words.sample
 end
+
+word = pick_word
+guessed_letters = Hash.new
 
 def get_guess
   puts "Please enter a single letter you did not guess yet:"
@@ -14,18 +14,18 @@ def get_guess
   end
 end
 
-def show_progress
+def show_progress(word, guessed_letters)
   word.chars.each {|char| 
   if guessed_letters.has_key?(char)
     print char
   else
     print '_'
   end
-  #$stdout.flush 
-}
+  }
+  return
 end
 
-
+puts show_progress(word, guessed_letters)
 
 
 
